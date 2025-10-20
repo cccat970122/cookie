19ab5cf05abfe1bd2255840cd1c35ddce808c8f1
cccat970122 <gary8662009@gmail.com>
2025-10-20 09:59:09 +0800

    Update login.php

--- login.php
+++ login.php
@@ -3,7 +3,7 @@ header('Content-Type: text/html; charset=utf-8');
 
 $users = [
     'admin' => 'password123',
-    'root'  => 'supersecretpassword' // 這個帳號的密碼你不需要給使用者
+    'root'  => 'supersecretpassword'
 ];
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
@@ -11,13 +11,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $password = $_POST['password'];
 
     if (isset($users[$username]) && $users[$username] === $password) {
-        // 成功登入
-        // 使用 Base64 編碼使用者名稱，並設定為 Cookie
         setcookie('sessionid', base64_encode($username), time() + 3600, "/");
-        header("Location: member.php"); // 導向會員頁面
+        header("Location: member.php");
         exit();
     } else {
         echo "<h1>登入失敗！</h1><p>帳號或密碼錯誤，請再試一次。</p>";
     }
 }
+
 ?>
